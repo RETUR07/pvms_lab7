e73f86be77bfdb0ed3cf5d45422c7a65c955230b
RETUR07 <mikita@intuswindows.com>
2023-02-01 00:21:40 +0300

    init

---
+++ lab7_2/app/src/main/java/com/task1/lab7_2/Maldivi.java
@@ -0,0 +1,14 @@
+package com.task1.lab7_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Maldivi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maldivi);
+    }
+}
